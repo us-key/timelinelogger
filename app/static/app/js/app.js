16a98fbd17f48a8285db9a87395c48414de9b4fa
us-key <y.omt0601@gmail.com>
2018-05-30 18:25:03 +0900

    #5 範囲指定でログの値があるセルの色付け表示

--- app/static/app/js/app.js
+++ app/static/app/js/app.js
@@ -3,3 +3,10 @@ $('.save').on('click', function(e){
 	$('.save').addClass('disabled');
 	$('#obj_form').submit();
 })
+$(function() {
+	$(".log_cell").each(function() {
+		if ($(this).text().trim() != "") {
+			$(this).addClass("table-success");
+		}
+	});
+});
