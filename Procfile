f0bdc79b944cdb34d165d7814451bbf70a719c62
us-key <y.omt0601@gmail.com>
2018-07-27 19:58:20 +0900

    herokuデプロイ用ファイル 途中

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn project.wsgi:application -b 0.0.0.0:$PORT
--- project/settings.py
+++ project/settings.py
@@ -16,7 +16,8 @@ from django.contrib.messages import constants as message_constants
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_ROOT = 'staticfiles'
+STATIC_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
