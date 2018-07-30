10537412c678427ff2c9e06624c8a4b732926404
us-key <y.omt0601@gmail.com>
2018-07-30 17:30:42 +0900

    heroku設定

--- project/wsgi.py
+++ project/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(get_wsgi_application())
